57f501ce6ff934eba7e57aee66bf98f360b78d8e
Jack <jackpeng.920@gmail.com>
2020-04-25 19:39:17 +0200

    rt

--- ClientRestGsbRapports/Form1.Designer.cs
+++ ClientRestGsbRapports/Form1.Designer.cs
@@ -46,47 +46,51 @@
             this.voirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.modifierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ajouterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtLogin
             // 
-            this.txtLogin.Location = new System.Drawing.Point(95, 86);
+            this.txtLogin.Location = new System.Drawing.Point(127, 106);
+            this.txtLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtLogin.Name = "txtLogin";
-            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.Size = new System.Drawing.Size(132, 22);
             this.txtLogin.TabIndex = 1;
             // 
             // LblLogin
             // 
             this.LblLogin.AutoSize = true;
-            this.LblLogin.Location = new System.Drawing.Point(12, 93);
+            this.LblLogin.Location = new System.Drawing.Point(16, 114);
+            this.LblLogin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LblLogin.Name = "LblLogin";
-            this.LblLogin.Size = new System.Drawing.Size(33, 13);
+            this.LblLogin.Size = new System.Drawing.Size(43, 17);
             this.LblLogin.TabIndex = 2;
             this.LblLogin.Text = "Login";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 140);
+            this.label2.Location = new System.Drawing.Point(16, 172);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.Size = new System.Drawing.Size(93, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Mot de passe";
             // 
             // txtMdp
             // 
-            this.txtMdp.Location = new System.Drawing.Point(95, 133);
+            this.txtMdp.Location = new System.Drawing.Point(127, 164);
+            this.txtMdp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMdp.Name = "txtMdp";
-            this.txtMdp.Size = new System.Drawing.Size(100, 20);
+            this.txtMdp.Size = new System.Drawing.Size(132, 22);
             this.txtMdp.TabIndex = 4;
             // 
             // btnValider
             // 
-            this.btnValider.Location = new System.Drawing.Point(95, 198);
+            this.btnValider.Location = new System.Drawing.Point(127, 244);
+            this.btnValider.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnValider.Name = "btnValider";
-            this.btnValider.Size = new System.Drawing.Size(75, 23);
+            this.btnValider.Size = new System.Drawing.Size(100, 28);
             this.btnValider.TabIndex = 5;
             this.btnValider.Text = "Valider";
             this.btnValider.UseVisualStyleBackColor = true;
@@ -98,13 +102,15 @@
             this.lblNom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.lblNom.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNom.Location = new System.Drawing.Point(15, 33);
+            this.lblNom.Location = new System.Drawing.Point(20, 41);
+            this.lblNom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNom.Name = "lblNom";
-            this.lblNom.Size = new System.Drawing.Size(2, 15);
+            this.lblNom.Size = new System.Drawing.Size(2, 19);
             this.lblNom.TabIndex = 6;
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fichierToolStripMenuItem,
             this.médecinsToolStripMenuItem,
@@ -114,7 +120,7 @@
             this.familleToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(448, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(597, 30);
             this.menuStrip1.TabIndex = 7;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -123,38 +129,38 @@
             this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.quitterToolStripMenuItem});
             this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
-            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(66, 26);
             this.fichierToolStripMenuItem.Text = "Fichier";
             // 
             // quitterToolStripMenuItem
             // 
             this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
-            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(138, 26);
             this.quitterToolStripMenuItem.Text = "Quitter";
             this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
             // 
             // médecinsToolStripMenuItem
             // 
             this.médecinsToolStripMenuItem.Name = "médecinsToolStripMenuItem";
-            this.médecinsToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.médecinsToolStripMenuItem.Size = new System.Drawing.Size(86, 26);
             this.médecinsToolStripMenuItem.Text = "Médecins";
             // 
             // rapportsToolStripMenuItem
             // 
             this.rapportsToolStripMenuItem.Name = "rapportsToolStripMenuItem";
-            this.rapportsToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.rapportsToolStripMenuItem.Size = new System.Drawing.Size(65, 26);
             this.rapportsToolStripMenuItem.Text = "Visites";
             // 
             // médicamentsToolStripMenuItem
             // 
             this.médicamentsToolStripMenuItem.Name = "médicamentsToolStripMenuItem";
-            this.médicamentsToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.médicamentsToolStripMenuItem.Size = new System.Drawing.Size(112, 26);
             this.médicamentsToolStripMenuItem.Text = "Médicaments";
             // 
             // visiteursToolStripMenuItem
             // 
             this.visiteursToolStripMenuItem.Name = "visiteursToolStripMenuItem";
-            this.visiteursToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.visiteursToolStripMenuItem.Size = new System.Drawing.Size(78, 26);
             this.visiteursToolStripMenuItem.Text = "Visiteurs";
             // 
             // familleToolStripMenuItem
@@ -164,43 +170,35 @@
             this.modifierToolStripMenuItem,
             this.ajouterToolStripMenuItem});
             this.familleToolStripMenuItem.Name = "familleToolStripMenuItem";
-            this.familleToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.familleToolStripMenuItem.Size = new System.Drawing.Size(70, 26);
             this.familleToolStripMenuItem.Text = "Famille";
             // 
             // voirToolStripMenuItem
             // 
             this.voirToolStripMenuItem.Name = "voirToolStripMenuItem";
-            this.voirToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.voirToolStripMenuItem.Size = new System.Drawing.Size(149, 26);
             this.voirToolStripMenuItem.Text = "Voir";
             this.voirToolStripMenuItem.Click += new System.EventHandler(this.voirToolStripMenuItem_Click);
             // 
             // modifierToolStripMenuItem
             // 
             this.modifierToolStripMenuItem.Name = "modifierToolStripMenuItem";
-            this.modifierToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.modifierToolStripMenuItem.Size = new System.Drawing.Size(149, 26);
             this.modifierToolStripMenuItem.Text = "Modifier";
             this.modifierToolStripMenuItem.Click += new System.EventHandler(this.modifierToolStripMenuItem_Click);
             // 
             // ajouterToolStripMenuItem
             // 
             this.ajouterToolStripMenuItem.Name = "ajouterToolStripMenuItem";
-            this.ajouterToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.ajouterToolStripMenuItem.Size = new System.Drawing.Size(149, 26);
             this.ajouterToolStripMenuItem.Text = "Ajouter";
             this.ajouterToolStripMenuItem.Click += new System.EventHandler(this.ajouterToolStripMenuItem_Click);
             // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(83, 48);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(333, 20);
-            this.dateTimePicker1.TabIndex = 8;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 261);
-            this.Controls.Add(this.dateTimePicker1);
+            this.ClientSize = new System.Drawing.Size(597, 321);
             this.Controls.Add(this.lblNom);
             this.Controls.Add(this.btnValider);
             this.Controls.Add(this.txtMdp);
@@ -210,6 +208,7 @@
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Gestion des rapports de visite";
             this.menuStrip1.ResumeLayout(false);
@@ -237,7 +236,6 @@
         private System.Windows.Forms.ToolStripMenuItem voirToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem modifierToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ajouterToolStripMenuItem;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
