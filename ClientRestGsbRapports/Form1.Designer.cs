6054b84838b1faf56b8891cf3919ab779960e2a5
marcel BRAGANCE <bragance.m@hotmail.fr>
2020-05-01 21:40:08 +0200

    designForm1

--- ClientRestGsbRapports/Form1.Designer.cs
+++ ClientRestGsbRapports/Form1.Designer.cs
@@ -44,19 +44,15 @@
             this.modifierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ajouterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.gtxtLogin = new Guna.UI.WinForms.GunaTextBox();
+            this.gtxtMdp = new Guna.UI.WinForms.GunaTextBox();
+            this.gunaTileButton1 = new Guna.UI.WinForms.GunaTileButton();
+            this.gbtnValider = new Guna.UI.WinForms.GunaButton();
             this.lblNom = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btnValider = new System.Windows.Forms.Button();
-            this.txtMdp = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtLogin = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // LblLogin
@@ -175,121 +171,145 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(72)))), ((int)(((byte)(114)))));
+            this.panel1.Controls.Add(this.gtxtLogin);
+            this.panel1.Controls.Add(this.gtxtMdp);
+            this.panel1.Controls.Add(this.gunaTileButton1);
+            this.panel1.Controls.Add(this.gbtnValider);
+            this.panel1.Controls.Add(this.lblNom);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 24);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(189, 483);
+            this.panel1.Size = new System.Drawing.Size(494, 263);
             this.panel1.TabIndex = 9;
             // 
-            // pictureBox1
+            // gtxtLogin
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(9, 141);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(170, 131);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.gtxtLogin.BaseColor = System.Drawing.Color.White;
+            this.gtxtLogin.BorderColor = System.Drawing.Color.Silver;
+            this.gtxtLogin.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gtxtLogin.FocusedBaseColor = System.Drawing.Color.White;
+            this.gtxtLogin.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(53)))), ((int)(((byte)(92)))));
+            this.gtxtLogin.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.gtxtLogin.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gtxtLogin.Location = new System.Drawing.Point(355, 85);
+            this.gtxtLogin.Name = "gtxtLogin";
+            this.gtxtLogin.PasswordChar = '\0';
+            this.gtxtLogin.SelectedText = "";
+            this.gtxtLogin.Size = new System.Drawing.Size(99, 26);
+            this.gtxtLogin.TabIndex = 23;
             // 
-            // panel2
+            // gtxtMdp
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.Info;
-            this.panel2.Controls.Add(this.lblNom);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.btnValider);
-            this.panel2.Controls.Add(this.txtMdp);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.txtLogin);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(185, 24);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(309, 483);
-            this.panel2.TabIndex = 10;
+            this.gtxtMdp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.gtxtMdp.BaseColor = System.Drawing.Color.White;
+            this.gtxtMdp.BorderColor = System.Drawing.Color.Silver;
+            this.gtxtMdp.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gtxtMdp.FocusedBaseColor = System.Drawing.Color.White;
+            this.gtxtMdp.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(53)))), ((int)(((byte)(92)))));
+            this.gtxtMdp.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.gtxtMdp.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gtxtMdp.Location = new System.Drawing.Point(355, 149);
+            this.gtxtMdp.Name = "gtxtMdp";
+            this.gtxtMdp.PasswordChar = '\0';
+            this.gtxtMdp.SelectedText = "";
+            this.gtxtMdp.Size = new System.Drawing.Size(99, 26);
+            this.gtxtMdp.TabIndex = 22;
+            // 
+            // gunaTileButton1
+            // 
+            this.gunaTileButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaTileButton1.AnimationSpeed = 0.03F;
+            this.gunaTileButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(72)))), ((int)(((byte)(114)))));
+            this.gunaTileButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaTileButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaTileButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaTileButton1.Font = new System.Drawing.Font("Segoe UI Light", 15.75F);
+            this.gunaTileButton1.ForeColor = System.Drawing.Color.White;
+            this.gunaTileButton1.Image = ((System.Drawing.Image)(resources.GetObject("gunaTileButton1.Image")));
+            this.gunaTileButton1.ImageSize = new System.Drawing.Size(160, 190);
+            this.gunaTileButton1.Location = new System.Drawing.Point(12, 15);
+            this.gunaTileButton1.Name = "gunaTileButton1";
+            this.gunaTileButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(72)))), ((int)(((byte)(114)))));
+            this.gunaTileButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaTileButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaTileButton1.OnHoverImage = null;
+            this.gunaTileButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaTileButton1.OnPressedDepth = 5;
+            this.gunaTileButton1.Size = new System.Drawing.Size(189, 160);
+            this.gunaTileButton1.TabIndex = 21;
+            this.gunaTileButton1.Text = "gunaTileButton1";
+            // 
+            // gbtnValider
+            // 
+            this.gbtnValider.AnimationHoverSpeed = 0.07F;
+            this.gbtnValider.AnimationSpeed = 0.03F;
+            this.gbtnValider.BackColor = System.Drawing.Color.Transparent;
+            this.gbtnValider.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.gbtnValider.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(53)))), ((int)(((byte)(92)))));
+            this.gbtnValider.BorderColor = System.Drawing.Color.Black;
+            this.gbtnValider.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gbtnValider.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gbtnValider.FocusedColor = System.Drawing.Color.Empty;
+            this.gbtnValider.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbtnValider.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(209)))), ((int)(((byte)(218)))));
+            this.gbtnValider.Image = ((System.Drawing.Image)(resources.GetObject("gbtnValider.Image")));
+            this.gbtnValider.ImageAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.gbtnValider.ImageSize = new System.Drawing.Size(25, 25);
+            this.gbtnValider.Location = new System.Drawing.Point(274, 205);
+            this.gbtnValider.Name = "gbtnValider";
+            this.gbtnValider.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(147)))), ((int)(((byte)(211)))));
+            this.gbtnValider.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gbtnValider.OnHoverForeColor = System.Drawing.Color.White;
+            this.gbtnValider.OnHoverImage = null;
+            this.gbtnValider.OnPressedColor = System.Drawing.Color.Black;
+            this.gbtnValider.Radius = 20;
+            this.gbtnValider.Size = new System.Drawing.Size(135, 36);
+            this.gbtnValider.TabIndex = 20;
+            this.gbtnValider.Text = "Connexion";
+            this.gbtnValider.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.gbtnValider.Click += new System.EventHandler(this.gbtnValider_Click_1);
             // 
             // lblNom
             // 
             this.lblNom.AutoSize = true;
-            this.lblNom.Location = new System.Drawing.Point(22, 80);
+            this.lblNom.Font = new System.Drawing.Font("Segoe UI Historic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(209)))), ((int)(((byte)(218)))));
+            this.lblNom.Location = new System.Drawing.Point(219, 24);
             this.lblNom.Name = "lblNom";
-            this.lblNom.Size = new System.Drawing.Size(0, 13);
-            this.lblNom.TabIndex = 8;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label3.Location = new System.Drawing.Point(18, 42);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 18);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Sign up";
-            // 
-            // btnValider
-            // 
-            this.btnValider.BackColor = System.Drawing.SystemColors.Highlight;
-            this.btnValider.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnValider.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnValider.Location = new System.Drawing.Point(22, 308);
-            this.btnValider.Margin = new System.Windows.Forms.Padding(2);
-            this.btnValider.Name = "btnValider";
-            this.btnValider.Size = new System.Drawing.Size(248, 28);
-            this.btnValider.TabIndex = 6;
-            this.btnValider.Text = "Valider";
-            this.btnValider.UseVisualStyleBackColor = false;
-            this.btnValider.Click += new System.EventHandler(this.btnValider_Click_1);
-            // 
-            // txtMdp
-            // 
-            this.txtMdp.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.txtMdp.Location = new System.Drawing.Point(22, 247);
-            this.txtMdp.Name = "txtMdp";
-            this.txtMdp.Size = new System.Drawing.Size(249, 20);
-            this.txtMdp.TabIndex = 5;
+            this.lblNom.Size = new System.Drawing.Size(0, 20);
+            this.lblNom.TabIndex = 19;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(21, 219);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Historic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(147)))), ((int)(((byte)(211)))));
+            this.label1.Location = new System.Drawing.Point(218, 155);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 17);
-            this.label1.TabIndex = 4;
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 18;
             this.label1.Text = "Mot de passe";
-            // 
-            // txtLogin
-            // 
-            this.txtLogin.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.txtLogin.Location = new System.Drawing.Point(22, 169);
-            this.txtLogin.Name = "txtLogin";
-            this.txtLogin.Size = new System.Drawing.Size(249, 20);
-            this.txtLogin.TabIndex = 3;
-         
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label4.Location = new System.Drawing.Point(19, 141);
+            this.label4.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(147)))), ((int)(((byte)(211)))));
+            this.label4.Location = new System.Drawing.Point(218, 85);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 17);
-            this.label4.TabIndex = 2;
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 17;
             this.label4.Text = "Login";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 507);
-            this.Controls.Add(this.panel2);
+            this.ClientSize = new System.Drawing.Size(494, 287);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.LblLogin);
@@ -301,9 +321,7 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -324,16 +342,14 @@
         private System.Windows.Forms.ToolStripMenuItem modifierToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ajouterToolStripMenuItem;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btnValider;
-        private System.Windows.Forms.TextBox txtMdp;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtLogin;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lblNom;
         private System.Windows.Forms.ToolStripMenuItem gérerLesMédicamentsToolStripMenuItem;
+        private Guna.UI.WinForms.GunaTextBox gtxtLogin;
+        private Guna.UI.WinForms.GunaTextBox gtxtMdp;
+        private Guna.UI.WinForms.GunaTileButton gunaTileButton1;
+        private Guna.UI.WinForms.GunaButton gbtnValider;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
